8003017cbf44cbb81b9b263cb62ff31c77996a15
Setoville <a6seto@edu.uwaterloo.ca>
2019-10-31 13:29:17 -0400

    Add routing framework

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import logo from './img/logo.jpg';
+import Menu from './Menu';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p id="coffee_title">
-           DOLLAR<h id="coffee_name">COFFEE</h>SHOP
-        </p>
-        <div class="horizontal_menu_container">
-          <div>Menu</div>
-          <div>Order</div>
-          <div>About</div>
-        </div>
-      </header>
-    </div>
-  );
-}
 
+class App extends React.Component {
+    render() {
+      return (
+        <div className="App">
+        <header className="App-header">
+          <p id="coffee_title">
+             DOLLAR<h id="coffee_name">COFFEE</h>SHOP
+          </p>
+          <div class="horizontal_menu_container">
+            <Router>
+              <div><Link to="./menu" class="horizontal_menu_item">Menu</Link></div>
+              <div><Link to="./order" class="horizontal_menu_item">Order</Link></div>
+              <div><Link to="./about" class="horizontal_menu_item">About</Link></div>
+            </Router>
+
+          </div>
+        </header>
+      </div>
+      );
+    }
+  }
 export default App;
+
